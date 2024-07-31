af16d701ddec3f2e3bff36468c530405a5750bbe
rajesh gawariya <rajeshhhhsingh@gmail.com>
2024-07-31 15:39:12 +0530

    update

--- 07ReactRouter/src/App.jsx
+++ 07ReactRouter/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +12,7 @@ function App() {
   return (
     <>
      
+      
     </>
   )
 }
